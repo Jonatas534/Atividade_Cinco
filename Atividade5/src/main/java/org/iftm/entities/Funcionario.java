f82c78b10059b6da9d4841a66e592540c2f6ccd1
Kluscan <92493522+Kluscan@users.noreply.github.com>
2022-10-23 09:21:43 -0300

    Elaboração do projeto e criação de alguns Testes

--- Atividade5/src/main/java/org/iftm/entities/Funcionario.java
+++ Atividade5/src/main/java/org/iftm/entities/Funcionario.java
@@ -1,34 +1,87 @@
 package org.iftm.entities;
 
 public class Funcionario {
-	private String nome;
-	private int horaTrabalhadas;
-	private double valorPorHora;
 
-	public Funcionario(String nome, int horaTrabalhadas, double valorPorHora) {
+	public static final double SALARIO_MINIMO = 1212.00;
+	public static final int HORAS_MAXIMO = 40;
+	public static final double VALORHORA_MAXIMO = 121.00;
+	public static final double VALORHORA_MINIMO = 48.48;
+	private String nome;
+	private  int horasTrabalhadas;
+	private double valorHora;
+	private double salario;
+	
+
+	public Funcionario(String nome, int horasTrabalhadas, double valorHora) {
+		
 		this.nome = nome;
-		this.horaTrabalhadas = horaTrabalhadas;
-		this.valorPorHora = valorPorHora;
+		this.horasTrabalhadas= validaHorasTrabalhadas (horasTrabalhadas);
+		this.valorHora = validaValorHora(valorHora);
+		calculaSalario();
+	}
+
+	public void calculaSalario() {
+		double valor = (valorHora * horasTrabalhadas * 4);
+		if (valor <1212) {
+			throw new IllegalArgumentException(
+					"O número de horas trabalhadas por funcionários próprios não deve ser menor que 48.48 nem maior 121.");
+		}
+			salario = valor;
+	}
+
+	public void setSalario(double salario) 
+	{
+		this.salario = (salario);
+		calculaSalario();
+	}
+
+	public String getNome() {
+		return nome;
 	}
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-	public void setHoraTrabalhadas(int horaTrabalhadas) {
-		this.horaTrabalhadas = horaTrabalhadas;
+	public int getHorasTrabalhadas() {
+		return horasTrabalhadas;
 	}
 
-	public void setValorPorHora(double valorPorHora) {
-		this.valorPorHora = valorPorHora;
+	public void setHorasTrabalhadas(int horasTrabalhadas) {
+		this.horasTrabalhadas = validaHorasTrabalhadas(horasTrabalhadas);
+		calculaSalario();
 	}
 
-	private int validaHorasTrabalhadas(int horasTrabalhadas) {
-		if (horasTrabalhadas > 40) {
+	public double getValorHora() {
+		return valorHora;
+	}
+
+	public void setValorHora(double valorHora) {
+		this.valorHora = validaValorHora(valorHora);
+	}
+	
+	public static double validaValorHora(double valorHora) {
+		if (valorHora >  VALORHORA_MAXIMO || valorHora < VALORHORA_MINIMO) {
+			throw new IllegalArgumentException(
+					"O número de horas trabalhadas por funcionários próprios não deve ser menor que 48.48 nem maior 121.");
+		}
+		return valorHora;
+	}
+
+	public static int validaHorasTrabalhadas(int horasTrabalhadas) {
+		if (horasTrabalhadas > HORAS_MAXIMO) {
 			throw new IllegalArgumentException(
 					"O número de horas trabalhadas por funcionários próprios deve ser menor ou igual a 40.");
 		}
 		return horasTrabalhadas;
 	}
+	
+	public boolean salarioValido(){
+		
+		return (salario >= SALARIO_MINIMO  ? true : false);
+	}
+	
 
 }
+
+
--- Atividade5/src/test/java/org/iftm/entities/FuncionarioTest.java
+++ Atividade5/src/test/java/org/iftm/entities/FuncionarioTest.java
@@ -1,35 +1,184 @@
 package org.iftm.entities;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
 public class FuncionarioTest {
 
+	@Test
 	public void  testarConstrutorPagamentoInvalido ( ) {
+		
+		//cenário de teste
+		String nomeValido = "Rogério Lima Duarte";
+		int horasTrabalhadasValidas = 3;
+		double valorHoraValido = 60.25;
+		boolean salarioValido = false;
+		Funcionario f;
+		
+		
+		
+		//execução	
+		Assertions.assertThrows(
+				IllegalArgumentException.class, 
+				()->{
+					new Funcionario(nomeValido,horasTrabalhadasValidas, valorHoraValido);
+					}
+				);
+		
+	}
+	
+	@Test
+	public void testarConstrutorEntradaValorHoraInvalida ( ) {
+		
+		//cenário de teste
+				String nomeValido = "Jonatas Silva";
+				int horasTrabalhadasValidas = 40;
+				double valorHoraValido = 122.00;
+				
+				//execução	
+				
+				Assertions.assertThrows(IllegalArgumentException.class, ()->{new Funcionario(nomeValido,horasTrabalhadasValidas, valorHoraValido);});
+					
+			}
+				
+		
+	
+	
+	public void testarConstrutorEntradaHorasInvalida ( ) {
+		//cenário de teste
+				String nomeValido = "Maria Peixoto gonçalves";
+				int horasTrabalhadasValidas = 42;
+				double valorHoraValido = 60.25;
+				boolean salarioValido = true;
+				Funcionario f;
+				
+				
+				//execução	
+				
+				f= new Funcionario(nomeValido,horasTrabalhadasValidas, valorHoraValido);
+				boolean resultadoObtido = f.salarioValido();
+				String nomeObtido = f.getNome();
+				int horasTrabalhadasObtida = f.getHorasTrabalhadas();
+				double valorHoraObtido= f. getValorHora();
+			
+				
+				
+				
+				
+				//comparação
+				
+				Assertions.assertEquals(salarioValido, resultadoObtido);
+				Assertions.assertEquals(nomeValido, nomeObtido);
+				Assertions.assertEquals(valorHoraValido, valorHoraObtido);
+				Assertions.assertEquals(horasTrabalhadasValidas, horasTrabalhadasObtida);
+		
+	}
+	
+	@Test
+	public void testarConstrutorEntradasValida ( ) {
+		
+		//cenário de teste
+				String nomeValido = "Rogério Lima Duarte";
+				double valorHoraValido = 60.00;
+				int horasTrabalhadasValidas = 40;
+				boolean salarioValido = true;
+				Funcionario f;
+				
+				
+				//execução	
+				
+				f= new Funcionario(nomeValido,horasTrabalhadasValidas, valorHoraValido);
+				boolean resultadoObtido = f.salarioValido();
+				String nomeObtido = f.getNome();
+				int horasTrabalhadasObtida = f.getHorasTrabalhadas();
+				double valorHoraObtido= f. getValorHora();
+				
+				
+				
+				
+				
+				//comparação
+				
+				Assertions.assertEquals(salarioValido, resultadoObtido);
+				Assertions.assertEquals(nomeValido, nomeObtido);
+				Assertions.assertEquals(valorHoraValido, valorHoraObtido);
+				Assertions.assertEquals(horasTrabalhadasValidas, horasTrabalhadasObtida);
+				
+			}
+		
+		
+		
+	
+	@Test
+	public void testarModificarHorasPagamentoInvalido ( ) {
+		//cenário de teste
+		String nomeValido = "João Peixoto Fagundes";
+		double valorHoraValido = 60.00;
+		int horasTrabalhadasValidas = 40;
+		Funcionario f;
+		f= new Funcionario(nomeValido,horasTrabalhadasValidas, valorHoraValido);
+		
+		
+		int novaHora = 3;
+		//execução	
+		Assertions.assertThrows(
+				IllegalArgumentException.class, 
+				()->{
+					f.setHorasTrabalhadas(novaHora);
+					}
+				);
+		
 		
 		
 	}
 
-	public void testarConstrutorEntradaValorHoraInvalida ( ) {
 		
 		
-	}
 	
-	public void testarConstrutorEntradaHorasInvalida ( ) {
 		
-		
-	}
 	
-	public void testarConstrutorEntradasValida ( ) {
-		
-	}
-	
-	public void testarModificarHorasPagamentoInvalido ( ) {
-		
-	}
-	
+	@Test
 	public void testarModificarHorasEntradaInvalida ( ) {
-		
+		//cenário de teste
+				String nomeValido = "João Peixoto Fagundes";
+				double valorHoraValido = 60.00;
+				int horasTrabalhadasValidas = 40;
+				Funcionario f;
+				f= new Funcionario(nomeValido,horasTrabalhadasValidas, valorHoraValido);
+				
+				
+				int novaHora = 42;
+				//execução	
+				Assertions.assertThrows(
+						IllegalArgumentException.class, 
+						()->{
+							f.setHorasTrabalhadas(novaHora);
+							}
+						);
+				
 	}
-	
+	@Test
 	public void testarModificarHorasEntradaValida ( ) {
+		
+		//cenário de teste
+		String nomeValido = "João Peixoto Fagundes";
+		double valorHoraValido = 49.00;
+		int horasTrabalhadasValidas = 40;
+		Funcionario f;
+		f= new Funcionario(nomeValido,horasTrabalhadasValidas, valorHoraValido);
+		
+		
+		int novaHora = 61;
+		//execução	
+		Assertions.assertThrows(
+				IllegalArgumentException.class, 
+				()->{
+					f.setHorasTrabalhadas(novaHora);
+					}
+				);
+		
 		
 	}
 	
